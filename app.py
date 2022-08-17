847ecab7f47173891455b8c86aaf68be3dbcde71
Kdohyoung <asd62126310@gmail.com>
2022-08-17 09:47:31 +0900

    8-17

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from config import Config
 from resources.user import UserLoginResource, UserLogoutResource, UserRegisterResource, jwt_blacklist
-from resources.operation  import prkOperation
 
 
 app = Flask(__name__)
@@ -13,7 +12,7 @@ app.config.from_object(Config)
 
 # JWT 토큰 라이브러리 만들기
 jwt = JWTManager(app)
-
+s
 # 로그아웃 된 토큰이 들어있는 set을, jwt 에 알려준다.
 @jwt.token_in_blocklist_loader
 def check_if_token_is_revoked(jwt_header, jwt_payload):
@@ -25,7 +24,6 @@ api = Api(app)
 api.add_resource(UserRegisterResource, '/users/register')
 api.add_resource(UserLoginResource, '/users/login')
 api.add_resource(UserLogoutResource, '/users/logout')
-api.add_resource(prkOperation, '/operation/<str:prk_center_id>')
 
 
 if __name__ == '__main__' :
