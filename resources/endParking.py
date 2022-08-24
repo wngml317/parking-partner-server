d8c4427e801dc8f8ffd036ee89ff8ca23e4925f2
Kdohyoung <asd62126310@gmail.com>
2022-08-24 16:46:15 +0900

    8-24

--- resources/endParking.py
+++ resources/endParking.py
@@ -18,7 +18,7 @@ class ParkingPayResource(Resource) :
             connection = get_connection()
 
             query = '''select p.id,p.prk_plce_nm,p.start_prk_at,p.prk_cmprt_co,timediff(now(),p.start_prk_at) AS use_prk_at, o.parking_chrge_bs_time,
-                        hour(timediff(now(),p.start_prk_at))*60 + minute(timediff(now(),p.start_prk_at)) as total_use_park_at,
+                        hour(timediff(now(),p.start_prk_at))*60 + minute(timediff(now(),p.start_prk_at)) as total_use_park_at,o.parking_chrge_one_day_chrge,
                         if ( hour(timediff(now(),p.start_prk_at))*60 + minute(timediff(now(),p.start_prk_at)) <= o.parking_chrge_bs_time, o.parking_chrge_bs_chrg, if(o.parking_chrge_adit_unit_time = 0, o.parking_chrge_bs_chrg,
                         (round((hour(timediff(now(),p.start_prk_at))*60 + minute(timediff(now(),p.start_prk_at)) - o.parking_chrge_bs_time) / o.parking_chrge_adit_unit_time) * o.parking_chrge_adit_unit_chrge) + o.parking_chrge_bs_chrg )) as end_pay
                         from parking p
@@ -52,3 +52,43 @@ class ParkingPayResource(Resource) :
         return { "result" : "success", 
                 "count" : len(result_list) ,
                 "items" : result_list }, 200
+
+
+
+    def put(self, parking_id) :
+        # 디비 업데이트 실행코드
+        try :
+            # 데이터 업데이트 
+            # 1. DB에 연결
+            connection = get_connection()
+
+             # 2. 쿼리문 만들기
+            query = '''update parking p 
+                        join operation o
+                        on p.prk_center_id = o.prk_center_id
+                        set use_prk_at = timediff(now(),p.start_prk_at) , end_pay = if ( hour(timediff(now(),p.start_prk_at))*60 + minute(timediff(now(),p.start_prk_at)) <= o.parking_chrge_bs_time, o.parking_chrge_bs_chrg, if(o.parking_chrge_adit_unit_time = 0, o.parking_chrge_bs_chrg,
+                        (round((hour(timediff(now(),p.start_prk_at))*60 + minute(timediff(now(),p.start_prk_at)) - o.parking_chrge_bs_time) / o.parking_chrge_adit_unit_time) * o.parking_chrge_adit_unit_chrge) + o.parking_chrge_bs_chrg ))
+                        where id = %s;'''
+                
+            record = (parking_id ,)
+
+            # 3. 커서를 가져온다.
+            cursor = connection.cursor()
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query, record)
+
+            # 5. 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
+            connection.commit()
+
+            # 6. 자원 해제
+            cursor.close()
+            connection.close()
+
+        except mysql.connector.Error as e :
+                print(e)
+                cursor.close()
+                connection.close()
+                return {'error' : str(e)}, 503
+
+        return {'result' :'success'}, 200
