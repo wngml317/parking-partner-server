b5bfdacdc16d756b0aeaae8573a0289bea61e511
wngml317 <wngml317@naver.com>
2022-08-24 14:25:56 +0900

    20220824

--- resources/parkComplete.py
+++ resources/parkComplete.py
@@ -60,10 +60,10 @@ class DetectTextResource(Resource) :
         for text in textDetections:
                 print ('Detected text:' + text['DetectedText'])
                 print ('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
-                print ('Id: {}'.format(text['Id']))
-                if 'ParentId' in text:
-                    print ('Parent Id: {}'.format(text['ParentId']))
-                print ('Type:' + text['Type'])
+                # print ('Id: {}'.format(text['Id']))
+                # if 'ParentId' in text:
+                #     print ('Parent Id: {}'.format(text['ParentId']))
+                # print ('Type:' + text['Type'])
                 print()
         return { 'img_prk' : Config.S3_LOCATION + new_file_name,
             'TextDetections' : textDetections}
