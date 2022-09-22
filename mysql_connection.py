3d1c43cbea2350c1ddd4f8c5068da4890cc7ec18
wngml317 <70615959+wngml317@users.noreply.github.com>
2022-09-22 10:17:23 +0900

    Update mysql_connection.py

--- mysql_connection.py
+++ mysql_connection.py
@@ -2,10 +2,10 @@ import mysql.connector
 
 def get_connection() :
     connection = mysql.connector.connect(
-        host = 'database-1.c8z17ubvxzpp.ap-northeast-2.rds.amazonaws.com',
-        database = 'parking_partner_db',
-        user = 'parking_partner_user',
-        password = '1234'
+        host = 'host',
+        database = 'database',
+        user = 'user',
+        password = 'password'
     )
 
     return connection
